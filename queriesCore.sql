a98984597fe546583b9022adcdc5cf0a853ae949
tvaltn <115498434+tvaltn@users.noreply.github.com>
2024-09-04 14:16:14 +0200

    Update queriesCore.sql

--- queriesCore.sql
+++ queriesCore.sql
@@ -2,3 +2,19 @@
 SELECT *
 FROM stations
 WHERE status = true
+
+-- Select stations from Marylebone to Piccadilly Circus that are closed
+SELECT *
+FROM stations
+WHERE id > 16 AND id < 20 AND status = false
+
+-- Select stations Elephant & Castle to Harrow & Wealdstone that have 'Network Rail' services (see the stations with the train in the image)
+SELECT s.name, sc.service
+FROM stations s
+LEFT JOIN services sc ON s.id = sc.stationid
+WHERE sc.service = 'Network Rail'
+
+-- Select stations from Lambeth North to Kensal Green
+SELECT name
+FROM stations
+WHERE id <= 24 AND id >= 9
