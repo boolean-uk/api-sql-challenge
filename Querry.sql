0380db5fc9e2d51eee758687f9d84d4a7af03796
Valen98 <leo.wahlandtcs@gmail.com>
2024-09-09 14:27:31 +0200

    Finished with travel time between stations

--- Querry.sql
+++ Querry.sql
@@ -1,5 +1,5 @@
 SELECT * FROM station WHERE isOpen = true;
-
+SELECT * FROM station;
 
 UPDATE station SET isOpen = false WHERE id = 12;
 
@@ -11,23 +11,23 @@ SELECT * FROM station WHERE isOpen = true;
 SELECT name, (SELECT name FROM station WHERE id = neighbor.station_id_2) FROM station LEFT JOIN neighbor on station.id = neighbor.station_id_1;
 
 
-SELECT name FROM station RIGHT JOIN neighbor on station.id = neighbor.station_id_1 WHERE id = neighbor.station_id_1 AND 
+SELECT name FROM station JOIN neighbor on station.id = neighbor.station_id_1 WHERE id = neighbor.station_id_1 AND 
     neighbor.station_id_2 > (SELECT id FROM station WHERE name='Kensal Green') AND neighbor.station_id_1 <= (SELECT ID FROM station WHERE name='Lambeth North');
   
 
-SELECT name FROM station RIGHT JOIN neighbor on station.id = neighbor.station_id_1 WHERE id = neighbor.station_id_1 AND 
+SELECT name FROM station JOIN neighbor on station.id = neighbor.station_id_1 WHERE id = neighbor.station_id_1 AND 
     neighbor.station_id_2 > (SELECT id FROM station WHERE name='Wembley Central') AND neighbor.station_id_1 <= (SELECT ID FROM station WHERE name='Oxford Circus');
 
 
 UPDATE station SET isOpen = false WHERE name = 'Baker Street';
 
 
-SELECT name FROM station RIGHT JOIN neighbor on station.id = neighbor.station_id_1 WHERE id = neighbor.station_id_1 AND neighbor.station_id_2 > (SELECT id FROM station WHERE name='Marylebone') AND neighbor.station_id_1 <= (SELECT ID from station WHERE name ='Piccadilly Circus') AND isOpen = false;
+SELECT name FROM station JOIN neighbor on station.id = neighbor.station_id_1 WHERE id = neighbor.station_id_1 AND neighbor.station_id_2 > (SELECT id FROM station WHERE name='Marylebone') AND neighbor.station_id_1 <= (SELECT ID from station WHERE name ='Piccadilly Circus') AND isOpen = false;
 
 SELECT *
   FROM station 
-    INNER JOIN neighbor on station.id = neighbor.station_id_1
-    INNER JOIN services on station.id = services.station_id
+    JOIN neighbor on station.id = neighbor.station_id_1
+    JOIN services on station.id = services.station_id
       WHERE station.id = neighbor.station_id_1 AND 
         neighbor.station_id_2 > (SELECT id FROM station WHERE name='Harrow & Wealdstone') AND 
         neighbor.station_id_1 <= (SELECT ID from station WHERE name ='Elephant and Castle') AND 
@@ -35,3 +35,15 @@ SELECT *
         services.hasTrainStation = true;
 
 --SELECT * FROM station LEFT JOIN neighbor on station.id = neighbor.station_id_2;
+
+
+SELECT * FROM neighbor;
+
+SELECT s1.name, s2.name, SUM(travel_time) FROM station 
+	JOIN neighbor n1 on station.id = n1.station_id_1 
+  JOIN station s1 on s1.name = 'Oxford Circus'
+  JOIN station s2 on s2.name = 'Waterloo'
+  WHERE station.id = n1.station_id_1 AND 
+    n1.station_id_2 > s1.id AND n1.station_id_1 <= s2.id
+    GROUP BY s1.name, s2.name;
+    
