1e8564bcd41fc24236a8c6242c4670e8124c0b39
Lowe Raivio <Lowe.Raivio@se.experis.com>
2025-01-29 10:57:07 +0100

    inital database tables, and ERD

---
+++ queries/create_tables.sql
@@ -0,0 +1,36 @@
+
+
+
+CREATE TABLE stations(
+	id serial PRIMARY KEY,
+	
+	lines 
+	
+	
+);
+
+
+CREATE TABLE zones(
+	id serial PRIMARY KEY, 
+	
+);
+
+
+CREATE TABLE station_in_zone(
+	id serial PRIMARY KEY, 
+	
+);
+
+
+CREATE TABLE mainLines(
+	id serial PRIMARY KEY, 
+	
+);
+
+
+
+
+CREATE TABLE services(
+	id serial PRIMARY KEY, 
+	
+);
