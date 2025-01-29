50fe4d5bbd37ecfe5abc412a8c386e3c9abee73f
Lowe Raivio <Lowe.Raivio@se.experis.com>
2025-01-29 11:44:59 +0100

    missing FK to lines in services, updated ERD

---
+++ queries/Insert.sql
@@ -0,0 +1,35 @@
+
+
+
+INSERT INTO zones
+(zone_name)
+VALUES
+;
+
+
+INSERT INTO stations
+(open_status, station_name, zone_id)
+VALUES
+;
+
+
+INSERT INTO lines
+(expected_duration_a_to_b, station_id, a_linked_station, b_linked_station)
+VALUES
+;
+
+INSERT INTO platforms
+(line_id, station_id)
+VALUES
+;
+
+INSERT INTO services
+(service_name, line_id)
+VALUES
+;
+
+
+INSERT INTO station_services
+(service_id, station_id)
+VALUES
+;
--- queries/create_tables.sql
+++ queries/create_tables.sql
@@ -1,44 +1,48 @@
 
 CREATE TABLE zones(
 	id serial PRIMARY KEY, 
-	name VARCHAR(128)
+	zone_name VARCHAR(128)
 );
 
 
 CREATE TABLE stations(
 	id serial PRIMARY KEY,
 	open_status boolean,
-	name VARCHAR(128),
+	station_name VARCHAR(128),
 	zone_id int,
 	CONSTRAINT fk_zone_id FOREIGN KEY (zone_id) REFERENCES zones(id),
-	UNIQUE(name)
+	UNIQUE(station_name)
 );
 
 
 CREATE TABLE lines(
 	id serial PRIMARY KEY, 
-	expected_duration time,
+	expected_duration_a_to_b time,
 	station_id int,
 	a_linked_station int,
-	b_linked_station int
+	b_linked_station int,
+	CONSTRAINT fk_a_linked_station FOREIGN KEY (a_linked_station) REFERENCES stations(id),
+	CONSTRAINT fk_b_linked_station FOREIGN KEY (b_linked_station) REFERENCES stations(id)
 );
 
 CREATE TABLE platforms(
 	line_id int,
 	station_id int,
-	CONSTRAINT fk_line_id FOREIGN KEY (line_id) REFERENCES lines(id)
+	CONSTRAINT fk_line_id FOREIGN KEY (line_id) REFERENCES lines(id),
 	CONSTRAINT fk_station_id FOREIGN KEY (station_id) REFERENCES stations(id)
 );
 
 CREATE TABLE services(
 	id serial PRIMARY KEY, 
-	name varchar(128)
+	service_name VARCHAR(128),
+	line_id int,
+	CONSTRAINT fk_line_id FOREIGN KEY (line_id) REFERENCES lines(id)
 );
 
 
 CREATE TABLE station_services(
 	service_id int,
 	station_id int,
-	CONSTRAINT fk_service_id FOREIGN KEY (service_id) REFERENCES services(id)
+	CONSTRAINT fk_service_id FOREIGN KEY (service_id) REFERENCES services(id),
 	CONSTRAINT fk_station_id FOREIGN KEY (station_id) REFERENCES stations(id)
 );
