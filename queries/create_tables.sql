33a61043f6f1aa70ca161778001340bb99a614e5
Lowe Raivio <Lowe.Raivio@se.experis.com>
2025-01-29 11:26:09 +0100

    changed design by adding junction tables

--- queries/create_tables.sql
+++ queries/create_tables.sql
@@ -1,36 +1,47 @@
 
-
-
 CREATE TABLE stations(
 	id serial PRIMARY KEY,
-	
-	lines 
-	
-	
+	open_status boolean,
+	name VARCHAR(128),
+	UNIQUE(name)
 );
-
 
 CREATE TABLE zones(
 	id serial PRIMARY KEY, 
-	
+	name VARCHAR(128)
 );
-
 
 CREATE TABLE station_in_zone(
-	id serial PRIMARY KEY, 
-	
+	zone_id int,
+	station_id int,
+	CONSTRAINT fk_zone_id FOREIGN KEY (zone_id) REFERENCES zones(id)
+	CONSTRAINT fk_station_id FOREIGN KEY (station_id) REFERENCES stations(id)
 );
 
-
-CREATE TABLE mainLines(
+CREATE TABLE lines(
 	id serial PRIMARY KEY, 
-	
+	expected_duration time,
+	station_id int,
+	a_linked_station int,
+	b_linked_station int
 );
 
-
-
+CREATE TABLE platforms(
+	line_id int,
+	station_id int,
+	CONSTRAINT fk_line_id FOREIGN KEY (line_id) REFERENCES lines(id)
+	CONSTRAINT fk_station_id FOREIGN KEY (station_id) REFERENCES stations(id)
+);
 
 CREATE TABLE services(
 	id serial PRIMARY KEY, 
-	
+	name varchar(128)
+);
+
+
+CREATE TABLE station_services(
+	service_id int,
+	station_id int,
+	CONSTRAINT fk_service_id FOREIGN KEY (service_id) REFERENCES services(id)
+	CONSTRAINT fk_station_id FOREIGN KEY (station_id) REFERENCES stations(id)
 );
